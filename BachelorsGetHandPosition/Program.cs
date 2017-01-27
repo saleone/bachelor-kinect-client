22dda0445f691f9005c8e260eedc857096316ca0
Sasa Savic <Sasa Savic@ELNOVO>
2017-01-27 19:29:57 +0100

    Add project files.

---
+++ BachelorsGetHandPosition/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Kinect;
+
+namespace BachelorsGetHandPosition
+{
+    class Program
+    {
+        public static KinectSensor sensor;
+
+        static void Main(string[] args)
+        {
+
+            KinectSensorCollection sensors = Microsoft.Kinect.
+
+        }
+    }
+}
