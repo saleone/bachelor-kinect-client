d4a40994e584aef5687e6a3ca5f08a18df05ac45
Sasa Savic <sasa@savic.pw>
2017-02-07 15:54:29 +0100

    Add basic functionality to collect hand position
    Start of the coordinate system for the hand position is the shoulder.
    We are only considering right side of the body and the first skeleton
    in the Kinects list of skeletons

--- BachelorsGetHandPosition/Program.cs
+++ BachelorsGetHandPosition/Program.cs
@@ -1,16 +1,120 @@
 ﻿using Microsoft.Kinect;
+using System;
+using System.Threading;
 
 namespace BachelorsGetHandPosition
 {
-    class Program
+    class GetHandPosition
     {
-        public static KinectSensor sensor;
+        public static KinectSensor sensor = null;
 
         static void Main(string[] args)
         {
+            sensor = GetKinectSensor();
+            int elevation;
+            try
+            {
+                elevation = int.Parse(args[1]);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                elevation = 15;
+            }
 
-            KinectSensorCollection sensors = Microsoft.Kinect.
+            if (elevation >= 20 && elevation <= 0)
+            {
+                Console.WriteLine();
+            }
+            
+
+            sensor.Start();
+
+
+            // When connected to the sensor dont let the application to close
+            while (true) ;
 
         }
+
+        static void Destruct()
+        {
+            if (sensor != null)
+            {
+                sensor.Stop();
+                sensor.AudioSource.Stop();
+            }
+
+        }
+
+        /// <summary>
+        /// Return initialized Kinect sensor object.
+        /// </summary>
+        /// <returns>KinectSensor</returns>
+        static KinectSensor GetKinectSensor()
+        {
+            Console.Write("Connecting to Kinect sensor");
+            KinectSensor _sensor = null;
+            while (_sensor == null || _sensor.Status == KinectStatus.Connected)
+            {
+                if (KinectSensor.KinectSensors.Count > 0)
+                {
+                    _sensor = KinectSensor.KinectSensors[0];
+                    if (_sensor.Status == KinectStatus.Connected)
+                    {
+                        _sensor.SkeletonStream.Enable();
+                        _sensor.SkeletonFrameReady += _sensor_SkeletonFrameReady;
+                        Console.WriteLine("\n Connected.");
+                        return _sensor;
+                    }
+
+                }
+                Console.Write(".");
+                Thread.Sleep(1000);
+            }
+
+            return _sensor;
+
+        }
+
+        /// <summary>
+        /// Handles skeleton changes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void _sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletonData = new Skeleton[6];
+            using (var skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame == null)
+                    return;
+                skeletonFrame.CopySkeletonDataTo(skeletonData);
+            }
+
+            Skeleton skeleton = null;
+            foreach (Skeleton s in skeletonData)
+            {
+                if (s.TrackingState == SkeletonTrackingState.Tracked)
+                    skeleton = s;
+            }
+
+            if (skeleton == null)
+                return;
+
+            Vector3 pos = GetHandPositionVector(skeleton);
+            Console.WriteLine(string.Format("Hand position vector is: ({0}, {1}, {2})", pos.X, pos.Y, pos.Z));
+
+        }
+
+        private static Vector3 GetHandPositionVector(Skeleton sk)
+        {
+            Joint shoulder = sk.Joints[JointType.ShoulderRight];
+            Joint hand = sk.Joints[JointType.HandRight];
+
+            Vector3 shoulderVec = new Vector3(shoulder.Position.X, shoulder.Position.Y, shoulder.Position.Z);
+            Vector3 handVec = new Vector3(hand.Position.X, hand.Position.Y, hand.Position.Z);
+
+            return handVec - shoulderVec;
+        }
+
     }
 }
---
+++ BachelorsGetHandPosition/Vector3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BachelorsGetHandPosition
+{
+    internal class Vector3
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public Vector3(float x, float y, float z)
+        {
+            X = (float) Math.Round(x, 2);
+            Y = (float) Math.Round(y, 2);
+            Z = (float) Math.Round(z, 2);
+        }
+
+        public static Vector3 operator +(Vector3 first, Vector3 second)
+        {
+            return new Vector3(first.X - second.X, first.Y - second.Y, first.Z - second.Z);
+        }
+
+        public static Vector3 operator -(Vector3 f, Vector3 s)
+        {
+            return new Vector3(f.X - s.X, f.Y - s.Y, f.Z - s.Z);
+        }
+
+    }
+}
